3589b2399ab4d4b8763a552b1993eeaed452b6f5
An Truong <antruong@Ans-MacBook-Pro.local>
2015-11-16 22:08:35 +0700

    Add search result page

--- src/assets/js/route.js
+++ src/assets/js/route.js
@@ -4,22 +4,38 @@ angular.module('iDocApp')
     .config(function ($stateProvider, $urlRouterProvider) {
 
         $stateProvider
-                .state({
-                    name: 'home',
-                    url: "/",
-                    views: {
-                        "content-view": {
-                            "controller": "HomeCtrl",
-                            "templateUrl": "idoc/home/home.html"
-                        }
-                    },
-                    resolve: {
-                        translatePartialLoader: function ($translate, $translatePartialLoader) {
-                            $translatePartialLoader.addPart('common');
-                            return $translate.refresh();
-                        }
+            .state({
+                name: 'home',
+                url: "/",
+                views: {
+                    "content-view": {
+                        "controller": "HomeCtrl",
+                        "templateUrl": "idoc/home/home.html"
                     }
-                })
+                },
+                resolve: {
+                    translatePartialLoader: function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('common');
+                        return $translate.refresh();
+                    }
+                }
+            })
+            .state({
+                name: 'result',
+                url: "/result",
+                views: {
+                    "content-view": {
+                        "controller": "ResultCtrl",
+                        "templateUrl": "idoc/result/result.html"
+                    }
+                } ,
+                resolve: {
+                    translatePartialLoader: function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('common');
+                        return $translate.refresh();
+                    }
+                }
+            })
 
         $urlRouterProvider.otherwise('/');
     });
