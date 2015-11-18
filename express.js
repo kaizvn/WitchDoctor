1a41e0c1c3259fa7eee19c30d9cfd5abe4eafee2
Hung H. Nguyen <hungnh1704@gmail.com>
2015-11-18 21:34:22 +0700

    - setup static server

---
+++ express.js
@@ -0,0 +1,26 @@
+/**
+ * Created by HungNguyen on 11/18/15.
+ */
+
+
+var express = require('express');
+var app = express();                               // create our app w/ express
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+// configuration =================
+
+app.use(express.static(__dirname + '/src/dist'));                 // set the static files location /public/img will be /img for users
+
+app.get('*', function(req, res) {
+    res.sendfile(__dirname + '/src/dist/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+app.use(bodyParser.urlencoded({'extended': 'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
