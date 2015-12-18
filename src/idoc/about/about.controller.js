c88dd09ff090957c58f6b2b5384a9bff723b0aff
Van An Truong <van-an.truong@fullsix.com>
2015-12-18 10:18:43 +0700

    Show/hide element in about page

--- src/idoc/about/about.controller.js
+++ src/idoc/about/about.controller.js
@@ -1,6 +1,18 @@
 "use strict";
 
 angular.module('iDocApp')
-    .controller('AboutCtrl', function () {
-        
+    .controller('AboutCtrl', function ($scope) {
+
+    	$scope.show = {
+    		about: true,
+        	team: false,
+        	faqs: false,
+        	contact: false
+    	}
+
+        $scope.showTab = function (tab) {
+        	_.each($scope.show, function (value, key, list) {
+        		list[key] = key === tab ? true : false;
+        	});
+        }
     });
